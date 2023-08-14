171d37802e1af8e583ca5abdef83a597f7537d4a
AngelLC18 <ciancioangel.epet20@gmail.com>
2023-08-14 10:03:08 -0300

    agrego alttausuario

---
+++ src/componentes/altausuario.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function altausuario(){
+    const [nombre, Setnombre] = useState ('')
+    const [apellido, Setapellido] = useState ('')
+    const [dni, Setdni] = useState ('')
+    const [genero, Setgenero] = useState ('')
+    const [edad, Setedad] = useState ('')
+}
+
