1ef5377f95720b98cb7aa6d650451a6ee409278b
AngelLC18 <ciancioangel.epet20@gmail.com>
2023-08-14 12:11:16 -0300

    agrego css

--- src/componentes/categorias.js
+++ src/componentes/categorias.js
@@ -1,14 +1,14 @@
 import React from "react";
 import category from "./tipodecat";
 import categorias from "./home"
+import categorias from "./categorias.css"
 const inicio = () => {
     return (
-        <div>
+        <div class="Categorias">
             <h1>Elija el tipo de categoria que quiera</h1>
             {categories.map((category) => (
         <CategoryBox key={category.id} category={category} />
       ))}
         </div>
-
     )
 }
