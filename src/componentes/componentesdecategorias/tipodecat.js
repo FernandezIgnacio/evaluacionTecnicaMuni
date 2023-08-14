a689dc3db8c6eab0831d44547c02c7e35b1a15c4
AngelLC18 <ciancioangel.epet20@gmail.com>
2023-08-14 10:45:09 -0300

    armo el compoenente categorias

--- src/componentes/componentesdecategorias/tipodecat.js
+++ src/componentes/componentesdecategorias/tipodecat.js
@@ -1,7 +1,14 @@
 import React from "react";
 const category = () => {
+    const UserProfileButton = () => {
+        const history = useHistory();
+        const goToUserProfile = () => {
+          history.push("/category"); 
+        };
+      };
+}  
     <div className="Categoria">
-        
+        return <button onClick={goToUserProfile}>Categoria</button>
     </div>
-}
+
 export default category
